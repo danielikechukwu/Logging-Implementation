674d1486ad3726a517f1c7be5e9ea0a7bf9b87e5
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-06-08 19:59:44 +0100

    refact(program): implement spacing in program file.

--- LoggingImplementation/Program.cs
+++ LoggingImplementation/Program.cs
@@ -9,14 +9,20 @@ builder.Services.AddSwaggerGen();
 
 // Configure logging provider
 builder.Logging.ClearProviders();
+
 builder.Logging.AddConsole();
+
 builder.Logging.AddDebug();
 
 // Set specific log levels for different librarues
 builder.Logging.SetMinimumLevel(LogLevel.Information); // Default minimum level
+
 builder.Logging.AddFilter("Microsoft", LogLevel.Warning); // For Microsoft.* namespaces
+
 builder.Logging.AddFilter("Microsoft.AspNetCore", LogLevel.Warning); // For ASP.NET Core
+
 builder.Logging.AddFilter("System", LogLevel.Warning); // For System.* namespaces
+
 builder.Logging.AddFilter("TestController.Namespace", LogLevel.Trace); // For your custom namespace
 
 var app = builder.Build();
